2f2e8fc95a4e91b34b0dea30e1bc0e3ca2dbf9ed
itsmeleeeo <fsleonardo91@gmail.com>
2023-02-08 12:40:35 -0800

    saving images on cloud, creating the like and dislike button and making it responsive

--- ClientApp/src/components/DashboardDeveloper.js
+++ ClientApp/src/components/DashboardDeveloper.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CardProfile from '../components/CardProfile'
 import Footer from './Footer'
 
 function DashboardDeveloper() {
+
+    const [username, setUsername] = useState();
+    const [userImage, setImage] = useState();
+    const [userPosition, setPosition] = useState();
+    const [userStack, setUserStack] = useState();
+    const [userbio, setUserBio] = useState();
 
     return (
         <div className="container">
